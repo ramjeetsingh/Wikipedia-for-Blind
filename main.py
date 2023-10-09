540cd741fd45c98e88bbe088d7e729427699e10a
Ramjeet Singh <ramjeet2k2@gmail.com>
2023-10-09 13:31:50 +0530

    implemented wikipedia seraching

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import speech_recognition as sr
+import wikipedia as wp
+import requests
+from bs4 import BeautifulSoup 
 
 r = sr.Recognizer()
+
+
+def search(wiki_link):
+    resp = requests.get(wiki_link)
+    
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.text,features="html.parser")
+
+        body_container = soup.find("div", attrs={'id': 'mw-content-text'})        
+        body = body_container.find("div", attrs={'class': 'mw-parser-output'})        #return the main div object
+
+        for p in body.find_all("p"):
+            print(p.get_text())
+
+    else:
+        print("Error in finding the webpage")
+
 
 with sr.Microphone() as source:
     print("Speak Anything:")
@@ -9,8 +29,11 @@ with sr.Microphone() as source:
 
     try:
         text = r.recognize_google(audio)
-        print(f"You said: {text}")
-    except sr.UnknownValueError:
+        print(f"Searching for: {text}")
+
+        page = wp.page(text)                                              #Returns the web page as an object
+        wikipedia_link = f'https://en.wikipedia.org/wiki/{page.title}'    #page link
+        search(wikipedia_link)
+
+    except:
         print("Could not recognize your voice")
-    except sr.RequestError:
-        print("hbefjnek")
